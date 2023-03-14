fa737819f23eec002be0ad6961d36d2d7760a561
Prestin Bell <prestin97@gmail.com>
2023-03-14 19:47:18 -0400

    added CSV reader iterator

--- src/test/java/TestDepartment.java
+++ src/test/java/TestDepartment.java
@@ -9,6 +9,8 @@ import static org.hamcrest.Matchers.*;
 public class TestDepartment {
     
 }
+/* 
+
 CSVReader reader = new CSVReaderBuilder(new FileReader("yourfile.csv")).build();    //"yourfile" is a variable and a place holder for the moment
      String [] nextLine;
      while ((nextLine = reader.readNext()) != null) 
@@ -16,3 +18,14 @@ CSVReader reader = new CSVReaderBuilder(new FileReader("yourfile.csv")).build();
         // nextLine[] is an array of values from the line
         System.out.println(nextLine[0] + nextLine[1] + "etc...");
      }
+
+//////////     or for an iterator//////////////////
+
+CSVReader reader = new CSVReaderBuilder(new FileReader("yourfile.csv")).build();
+     CSVIterator iterator = new CSVIterator(reader);
+     for(String[] nextLine : iterator) 
+     {
+        // nextLine[] is an array of values from the line
+        System.out.println(nextLine[0] + nextLine[1] + "etc...");
+     }
+*/
