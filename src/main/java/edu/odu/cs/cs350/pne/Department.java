242439c1598269292e7bfc885b06236d6a3e597f
Prestin Bell <prestin97@gmail.com>
2023-03-14 19:50:39 -0400

    fix

--- src/main/java/edu/odu/cs/cs350/pne/Department.java
+++ src/main/java/edu/odu/cs/cs350/pne/Department.java
@@ -2,3 +2,23 @@ package edu.odu.cs.cs350.pne;
 public class Department {
     
 }
+/* 
+
+CSVReader reader = new CSVReaderBuilder(new FileReader("yourfile.csv")).build();    //"yourfile" is a variable and a place holder for the moment
+     String [] nextLine;
+     while ((nextLine = reader.readNext()) != null) 
+     {
+        // nextLine[] is an array of values from the line
+        System.out.println(nextLine[0] + nextLine[1] + "etc...");
+     }
+
+//////////or for an iterator//////////////////
+
+CSVReader reader = new CSVReaderBuilder(new FileReader("yourfile.csv")).build();
+     CSVIterator iterator = new CSVIterator(reader);
+     for(String[] nextLine : iterator) 
+     {
+        // nextLine[] is an array of values from the line
+        System.out.println(nextLine[0] + nextLine[1] + "etc...");
+     }
+*/
