612d846dfb886aca2c12c88f777b16f907d03945
Ray Saleh <ray.saleh67@gmail.com>
2023-03-15 20:29:31 -0400

    Snapshot comments added

--- src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
+++ src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
@@ -2,3 +2,20 @@ package edu.odu.cs.cs350.pne;
 public class Snapshot {
     
 }
+/*
+                            //idea of how to write in data from files found from   https://www.w3schools.com/java/java_files_create.asp
+public class WriteToFile {
+  public static void main(String[] args) {
+    try {
+      FileWriter myWriter = new FileWriter("filename.txt");
+      myWriter.write("Files in Java might be tricky, but it is fun enough!");
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
+
+*/
