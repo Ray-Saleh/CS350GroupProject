da4bf0f884999f7f65ce46dc438601207032a933
Ray Saleh <ray.saleh67@gmail.com>
2023-03-17 14:03:23 -0400

    Test 1 for offering(Constructor)

--- project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -2,10 +2,30 @@ package edu.odu.cs.cs350.pne;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 
 public class TestOffering {
+
+@Test
+public void testConstructor(){
+    Offering o = new Offering();
+    
+
+    assertThat(o.getCourseName(), is(""));
+    assertTha(o.getTime(), is(""));
+    assertThat(o.getSeats(), is(""));
+    assertThat(o.getEnrollment(), is(""));
+    assertThat(o.getMaxCap(), is(""));
+    
+
+
+}
+
+
     
 }
