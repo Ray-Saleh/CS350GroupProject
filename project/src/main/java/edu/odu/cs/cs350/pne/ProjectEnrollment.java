b5138a586a60eed66bb5613df136462899823259
ChanceSadler <nsadl001@odu.edu>
2023-03-16 21:54:44 -0400

    Minor Clean up from previous testing

--- project/src/main/java/edu/odu/cs/cs350/pne/ProjectEnrollment.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/ProjectEnrollment.java
@@ -2,10 +2,4 @@ package edu.odu.cs.cs350.pne;
 
 public class ProjectEnrollment {
    
-   
-    public static void main(String[]args){
-
-        Department d;
-        d.ListingDirectory();
-    }
 }
