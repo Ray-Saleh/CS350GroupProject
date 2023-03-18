29617072565d4d393d87f21fb63fb55f93e9bf6d
Ray Saleh <ray.saleh67@gmail.com>
2023-03-18 18:20:15 -0400

    Worked on Snapshot.java to our understanding (Probably wrong lol) Co-authored-by: Prestin Bell

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
@@ -5,14 +5,44 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-import edu.odu.cs.cs350.pne.*;
 
 public class TestSnapshot {
     
     @Test
     public void testConstructor()
     {
+        Snapshot s = new Snapshot();
+
+        assertThat(s.getYear(), is(0));
+        assertThat(s.getSemester(), is(0));
+    }
+    
+    @Test
+    public void testCopyConstructor(){
+        Snapshot s = new Snapshot(2020,10);
+
+        assertThat(s.getYear(), is(2020));
+        assertThat(s.getSemester(),is(10));
+    }
+
+    @Test
+    public void setSemester(){
+        Snapshot s = new Snapshot(2020,10);
+
+        s.setSemester(20);
         
+        assertThat(s.getYear(), is(2020));
+        assertThat(s.getSemester(),is(20));
+    }
+
+    @Test
+    public void SetYear(){
+        Snapshot s = new Snapshot(2020,10);
+
+        s.setYear(2030);
+        
+        assertThat(s.getYear(), is(2030));
+        assertThat(s.getSemester(),is(10));
     }
 
 
