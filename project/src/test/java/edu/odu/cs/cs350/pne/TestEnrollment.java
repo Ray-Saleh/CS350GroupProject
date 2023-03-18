73eeca33e20cb6fc33111f9a9f6bce215ca3732a
Scott Atkins <satki002@odu.edu>
2023-03-18 03:08:37 -0400

    TestEnrollment V1.0

--- project/src/test/java/edu/odu/cs/cs350/pne/TestEnrollment.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestEnrollment.java
@@ -8,5 +8,17 @@ import static org.hamcrest.Matchers.*;
 import edu.odu.cs.cs350.pne.*;
 
 public class TestEnrollment {
+
+    @Test
+public void testConstructor(){
+    Enrollment enrollment=new Enrollment();
+    assertThat(enrollment.getXLSTCap(), equalTo(0));
+    assertThat(enrollment.getENR(), equalTo(0));
+    assertThat(enrollment.getLINK(), equalTo(0));
+    assertThat(enrollment.getXLSTGroup(), equalTo(0));
+    assertThat(enrollment.getOVERALLCAP(), equalTo(0));
+    assertThat(enrollment.getXLSTENR(), equalTo(0));
+    assertNull(enrollment.getCourse());
+}
     
 }
