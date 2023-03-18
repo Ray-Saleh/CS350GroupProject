ea9cabe85074371eff3e558d68e3ebed72bada49
Prestin Bell <prestin97@gmail.com>
2023-03-18 17:45:25 -0400

    section.java

--- project/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -4,9 +4,65 @@ public class Section
 {
     int crn;
     int link;
-    int enrollment;
+    int eStudent;   //"enrolled Student"
     String course;
-    int XLSTCap;
+    int XLSTCap;    //max number of students to each section
 
-    
+    public class Course
+    {
+        private String subject;
+        private int crn;
+        
+        public Course()
+        {
+            this.subject = "";
+            this.crn = 0;
+        }
+        public Course(String s, int c)
+        {
+            this.subject = s;
+            this.crn = c;
+        }
+    }
+    public class crn
+    {
+        private int classCrn;
+
+        public crn()
+        {
+            this.classCrn = 0;
+        }
+        public crn(int n)
+        {
+            this.classCrn = n;
+        }
+    }
+
+    public class eStudent   //"enrolled Student"
+    {
+        private String name;
+        
+        public eStudent()
+        {
+            this.name = "";
+        }
+        public eStudent(String a)
+        {
+            this.name = a;
+        }
+    }
+        public class XLSTCap
+        {
+            private int cap;
+
+            public XLSTCap()
+            {
+                this.cap = 0;
+            }
+            public XLSTCap(int p)
+            {
+                this.cap = p;
+            }
+        }
+
 }
--- project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
@@ -32,7 +32,6 @@ public class TestCourse
         assertThat(testCourse.getCRSE(), is(350));
     }
 
-<<<<<<< HEAD
     @Test
     
         assertThat(c.getCourseName(), is(""));
@@ -40,8 +39,4 @@ public class TestCourse
         assertThat(c.getSemester(), is (""));
         assertThat(c.getInstructor(), is (""));     //instructor/professor/teacher, whichever is the appropiate name
     }
-=======
-
-
->>>>>>> b719334ec29f14c9b5da86136544ef72e8a25173
 }
