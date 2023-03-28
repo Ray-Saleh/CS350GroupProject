950c7505f9206b25dabc67c5fa93dc2bd4c3a4a5
Prestin Bell <prestin97@gmail.com>
2023-03-28 15:32:12 -0400

    input 3,4,5 for test

--- project/src/test/java/edu/odu/cs/cs350/pne/TestDepartment.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestDepartment.java
@@ -33,6 +33,18 @@ public class TestDepartment {
         int[] input2 = {1, 2, 3, 4};
         int[] expectedOutput2 = {1, 2, 3, 3};
         assertArrayEquals(expectedOutput2, d.smoothCurve(input2, 1));
+
+        int[] input3 = {1, 2, 3, 4, 5, 6};
+        int[] expectedOutput3 = {1, 2, 3, 4, 5, 5};
+        assertArrayEquals(expectedOutput3, d.smoothCurve(input3, 1));
+
+        int[] input4 = {1, 2, 3, 4, 5};
+        int[] expectedOutput4 = {1, 2, 3, 4, 4};
+        assertArrayEquals(expectedOutput4, d.smoothCurve(input4, 1));
+
+        int[] input5 = {1, 2, 3, 4, 5, 6};
+        int[] expectedOutput5 = {1, 2, 3, 4, 5, 5};
+        assertArrayEquals(expectedOutput5, d.smoothCurve(input5, 1));
     }
 
 }
