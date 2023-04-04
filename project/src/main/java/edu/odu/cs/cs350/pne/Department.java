1915c71da8689aa904d8176755c06f22bb0fdace
RaysMind <ray.saleh67@gmail.com>
2023-04-04 19:18:01 -0400

    updated

--- project/src/main/java/edu/odu/cs/cs350/pne/Department.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Department.java
@@ -1,6 +1,7 @@
 package edu.odu.cs.cs350.pne;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,45 +15,95 @@ import java.util.ArrayList;
 
 public class Department {
 
-    // private String subject;
-    // Pathname will be an incoming arg
-    // List of semesters with their location will be an arg
-    // Location of output will be an arg
-    // Optionally a date in format of YYYY-MM-DD will be an arg
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Usage: java CsvReader <directory>");
+            System.exit(1);
+        }
 
-        Scanner userInput = new Scanner(System.in);
-        String inputDirectory = new String("");
-        Enrollment e = new Enrollment();
+        // fake output
+        String filename = "Fake_Output.csv";
+        File file = new File(filename);
+        if (file.exists()) {
+            file.delete();
+            System.out.println("Deleted existing file " + filename);
+        }
+        String[] data1 = { "CRN", "Subj", "CRSE", "XLST Cap" };
+        String[] data2 = { "1", "CS", "330", "30" };
+        String[] data3 = { "2", "CS", "350", "30" };
+        String[] data4 = { "3", "CS", "361", "30" };
+        writeDataToCSV(data1, "Fake_Output.csv");
+        writeDataToCSV(data2, "Fake_Output.csv");
+        writeDataToCSV(data3, "Fake_Output.csv");
+        writeDataToCSV(data4, "Fake_Output.csv");
+        // comment from here to fake output comment to remove the fakeoutput
 
-        System.out.println("Enter directory path to semesters:");
-        getDirectory(userInput.nextLine(), inputDirectory);
+        String directory = args[0];
 
-        readCSV(inputDirectory);
-        System.out.println("Enter directory path for data Output:");
-        getEnrollment(e);
-        detailedProjecitonReport(userInput.nextLine());
-
-        userInput.close();
-    }
-    // test
-
-    public static void readCSV(String inputPathname) {
-        // Go to specified directory
-        // Check if Dates.txt is there if not abort
-        // The folders contain the snapshots
-
+        readCsvFiles(directory);
     }
 
-    // Summery Projection written as standard output
-    public static void summeryProjectionReport(Enrollment enrolled) {
-        System.out.println(" of enrollment period has elapsed.");
-        System.out.println("Course Enrollment Projected Cap");
+    public static void readCsvFiles(String directory) {
+        File dir = new File(directory);
 
-        // for (Course c : enrolled.course )
-        System.out.println(enrolled.getCourse() + " " + enrolled.getENR() + " " + enrolled.getENR() + " "
-                + enrolled.getOVERALLCAP());
+        if (!dir.isDirectory()) {
+            System.err.println("Error: " + directory + " is not a directory.");
+            System.exit(1);
+        }
 
+        File[] files = dir.listFiles((dir1, name) -> name.endsWith(".csv"));
+
+        for (File file : files) {
+            try {
+                Scanner scanner = new Scanner(file);
+                scanner.useDelimiter(",");
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] fields = line.split(",");
+                    // Do something with the fields
+                }
+
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                System.err.println("Error: File not found - " + file.getName());
+                System.exit(1);
+            }
+        }
+    }
+
+    public static int[] smoothCurve(int[] values, int windowSize) {
+        int[] smoothedValues = new int[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+            int sum = 0;
+            int count = 0;
+            for (int j = Math.max(0, i - windowSize); j <= Math.min(values.length - 1, i + windowSize); j++) {
+                sum += values[j];
+                count++;
+            }
+            smoothedValues[i] = sum / count;
+        }
+
+        return smoothedValues;
+    }
+
+    public static void writeDataToCSV(String[] data, String filename) {
+        try {
+            FileWriter csvWriter = new FileWriter(filename, true);
+            for (int i = 0; i < data.length; i++) {
+                csvWriter.append(data[i]);
+                if (i != data.length - 1) {
+                    csvWriter.append(","); // use a comma as the delimiter between columns
+                }
+            }
+            csvWriter.append("\n"); // add a new line character to separate rows
+            csvWriter.flush();
+            csvWriter.close();
+            System.out.println("Data has been written to " + filename + " successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // Detailed Projection Report outputed to a CSV file
@@ -77,45 +128,90 @@ public class Department {
         }
     }
 
-    // Do the calculations and output to specified folder
-    public static void getEnrollment(Enrollment enroll) {
-
-    }
-
-    public static void getDirectory(String inputPathname, String savedPathname) {
-        File directory = new File(inputPathname);
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile()) {
-                    System.out.println(file.getName());
-                } else if (file.isDirectory()) {
-                    System.out.println(file.getName());
-                }
-            }
-            savedPathname = inputPathname;
-        }
-
-    }
-
-    public static int[] smoothCurve(int[] values, int windowSize) {
-        int[] smoothedValues = new int[values.length];
-
-        for (int i = 0; i < values.length; i++) {
-            int sum = 0;
-            int count = 0;
-            for (int j = Math.max(0, i - windowSize); j <= Math.min(values.length - 1, i + windowSize); j++) {
-                sum += values[j];
-                count++;
-            }
-            smoothedValues[i] = sum / count;
-        }
-
-        return smoothedValues;
-    }
 }
-
 /*
+ * // private String subject;
+ * // Pathname will be an incoming arg
+ * // List of semesters with their location will be an arg
+ * // Location of output will be an arg
+ * // Optionally a date in format of YYYY-MM-DD will be an arg
+ * public static void main(String[] arg) {
+ * 
+ * Scanner userInput = new Scanner(System.in);
+ * String inputDirectory = new String("");
+ * Enrollment e = new Enrollment();
+ * 
+ * System.out.println("Enter directory path to semesters:");
+ * getDirectory(userInput.nextLine(), inputDirectory);
+ * 
+ * readCSV(inputDirectory);
+ * System.out.println("Enter directory path for data Output:");
+ * getEnrollment(e);
+ * detailedProjecitonReport(userInput.nextLine());
+ * 
+ * userInput.close();
+ * }
+ * // test
+ * 
+ * public static void readCSV(String inputPathname) {
+ * // Go to specified directory
+ * // Check if Dates.txt is there if not abort
+ * // The folders contain the snapshots
+ * 
+ * }
+ * 
+ * // Summery Projection written as standard output
+ * public static void summeryProjectionReport(Enrollment enrolled) {
+ * System.out.println(" of enrollment period has elapsed.");
+ * System.out.println("Course Enrollment Projected Cap");
+ * 
+ * // for (Course c : enrolled.course )
+ * System.out.println(enrolled.getCourse() + " " + enrolled.getENR() + " " +
+ * enrolled.getENR() + " "
+ * + enrolled.getOVERALLCAP());
+ * 
+ * }
+ * 
+ * // Do the calculations and output to specified folder
+ * public static void getEnrollment(Enrollment enroll) {
+ * 
+ * }
+ * 
+ * public static void getDirectory(String inputPathname, String savedPathname) {
+ * File directory = new File(inputPathname);
+ * File[] files = directory.listFiles();
+ * if (files != null) {
+ * for (File file : files) {
+ * if (file.isFile()) {
+ * System.out.println(file.getName());
+ * } else if (file.isDirectory()) {
+ * System.out.println(file.getName());
+ * }
+ * }
+ * savedPathname = inputPathname;
+ * }
+ * 
+ * }
+ * 
+ * public static int[] smoothCurve(int[] values, int windowSize) {
+ * int[] smoothedValues = new int[values.length];
+ * 
+ * for (int i = 0; i < values.length; i++) {
+ * int sum = 0;
+ * int count = 0;
+ * for (int j = Math.max(0, i - windowSize); j <= Math.min(values.length - 1, i
+ * + windowSize); j++) {
+ * sum += values[j];
+ * count++;
+ * }
+ * smoothedValues[i] = sum / count;
+ * }
+ * 
+ * return smoothedValues;
+ * }
+ * }
+ * 
+ * /*
  * public static List<String[]> readCsvFiles(String directoryPath) throws
  * IOException
  * {
