4553eb5c87507671e46617acb07d38deff75ac71
RaysMind <ray.saleh67@gmail.com>
2023-04-05 11:29:18 -0400

    update

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
@@ -1,4 +1,5 @@
 package edu.odu.cs.cs350.pne;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,12 +9,10 @@ import static org.hamcrest.Matchers.*;
 import edu.odu.cs.cs350.pne.*;
 
 public class TestSemester {
-    
-    @Test
-    public void testConstructor()
-    {
-        
-    }
 
+    @Test
+    public void testConstructor() {
+
+    }
 
 }
