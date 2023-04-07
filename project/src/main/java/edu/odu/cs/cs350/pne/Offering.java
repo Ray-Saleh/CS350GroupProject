b3467cd43b887afb8dafd75b6dc3f6c0fc7fb907
RaysMind <ray.saleh67@gmail.com>
2023-04-07 11:41:22 -0400

    fixed TestOfferings

--- project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
@@ -1,68 +1,62 @@
 package edu.odu.cs.cs350.pne;
 
 public class Offering {
-    //numsecotions will be int for now
-    //professor is string
-    //overallcap and overall enrollment will be apart of enrollment
-    //course will be an object of course
+    // numsecotions will be int for now
+    // professor is string
+    // overallcap and overall enrollment will be apart of enrollment
+    // course will be an object of course
 
     private int crn;
     private String professor;
     private int numSections;
     private Enrollment enrollment;
-   
 
-    public Offering(){
-        crn = 0;
+    public Offering() {
+        this.crn = 0;
         this.professor = "";
         this.numSections = 0;
         this.enrollment = new Enrollment();
-      
+
     }
-    public Offering(int inCRN,String p, int n, Enrollment e){
+
+    public Offering(int inCRN, String p, int n, Enrollment e) {
         this.crn = inCRN;
         this.professor = p;
         this.numSections = n;
         this.enrollment = e;
-       
+
     }
 
-	public String getProfessor() {
-		return this.professor;
-	}
+    public String getProfessor() {
+        return this.professor;
+    }
 
     public int getNumSections() {
         return this.numSections;
     }
 
-
-	public void setProfessor(String string) {
+    public void setProfessor(String string) {
         this.professor = string;
-	}
-	public void setNumSections(int i) {
+    }
+
+    public void setNumSections(int i) {
         this.numSections = i;
-	}
-
-    public void setCRN(int inCRN){
-        crn = inCRN;
     }
 
-    public int getCRN(){
-        return crn; 
+    public void setCRN(int inCRN) {
+        this.crn = inCRN;
     }
 
-    public void setEnrollment(Enrollment inEnrollment)
-    {
-        enrollment = inEnrollment;
+    public int getCRN() {
+        return this.crn;
     }
 
-    public Enrollment getEnrollment(){
-        return enrollment;
+    public void setEnrollment(Enrollment inEnrollment) {
+        this.enrollment = inEnrollment;
     }
 
+    public Enrollment getEnrollment() {
+        return this.enrollment;
+    }
 
-
-
-
-    
 }
--- project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -1,4 +1,5 @@
 package edu.odu.cs.cs350.pne;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,52 +7,82 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-
 public class TestOffering {
 
     @Test
-    public void testConstructor(){
+    public void testConstructor() {
         Offering o = new Offering();
+
+        assertThat(o.getCRN(), is(0));
         assertThat(o.getProfessor(), is(""));
         assertThat(o.getNumSections(), is(0));
-    
+        assertNotNull(o.getEnrollment());
+
     }
 
     @Test
-    public void testCConstructor(){
-     
-        Offering o = new Offering(1,"Zeil", 5, new Enrollment());
+    public void testCConstructor() {
+        Enrollment e = new Enrollment(0, 0, "", 0, 0, new Course());
+        Offering o = new Offering(1234, "Zeil", 5, e);
 
+        assertThat(o.getCRN(), is(1234));
         assertThat(o.getProfessor(), is("Zeil"));
         assertThat(o.getNumSections(), is(5));
-
+        assertThat(o.getEnrollment(), is(e));
     }
 
     @Test
-    public void testSetProfessor(){
-       
-       
-        Offering o = new Offering(1,"Zeil", 5, new Enrollment());
-       
+    public void testSetProfessor() {
+        Enrollment e = new Enrollment(0, 0, "", 0, 0, new Course());
+        Offering o = new Offering(1234, "Zeil", 5, e);
+
         o.setProfessor("Kennedy");
+
+        assertThat(o.getCRN(), is(1234));
         assertThat(o.getProfessor(), is("Kennedy"));
         assertThat(o.getNumSections(), is(5));
-
+        assertThat(o.getEnrollment(), is(e));
     }
 
-
-
-
-
     @Test
-    public void testSetEnrollmentNum(){
-       
+    public void testSetCRN() {
+        Enrollment e = new Enrollment(0, 0, "", 0, 0, new Course());
+        Offering o = new Offering(1234, "Zeil", 5, e);
 
-        Offering o = new Offering(1,"Zeil", 5, new Enrollment());
-        o.setCRN(20);
+        o.setCRN(11);
+
+        assertThat(o.getCRN(), is(11));
         assertThat(o.getProfessor(), is("Zeil"));
         assertThat(o.getNumSections(), is(5));
-      
+        assertThat(o.getEnrollment(), is(e));
+    }
+
+    @Test
+    public void testSetNumSections() {
+        Enrollment e = new Enrollment(0, 0, "", 0, 0, new Course());
+        Offering o = new Offering(1234, "Zeil", 5, e);
+
+        o.setNumSections(0);
+
+        assertThat(o.getCRN(), is(1234));
+        assertThat(o.getProfessor(), is("Zeil"));
+        assertThat(o.getNumSections(), is(0));
+        assertThat(o.getEnrollment(), is(e));
+    }
+
+    @Test
+    public void testSetEnrollment() {
+        Enrollment e = new Enrollment(0, 0, "", 0, 0, new Course());
+        Offering o = new Offering(1234, "Zeil", 5, e);
+
+        Enrollment e1 = new Enrollment(10, 10, "Apples", 10, 10, new Course());
+
+        o.setEnrollment(e1);
+
+        assertThat(o.getCRN(), is(1234));
+        assertThat(o.getProfessor(), is("Zeil"));
+        assertThat(o.getNumSections(), is(5));
+        assertThat(o.getEnrollment(), is(e1));
     }
 
 }
