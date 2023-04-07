60e784c5e90dc9f0e7613c33b1b85ca6a48113cb
RaysMind <ray.saleh67@gmail.com>
2023-04-06 20:38:31 -0400

    Finished TestCourse

--- project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
@@ -22,4 +22,59 @@ public class TestCourse {
 
     }
 
+    @Test
+    public void testCopyConstructor() {
+        Offering o = new Offering();
+        ArrayList<Offering> offerings = new ArrayList<Offering>();
+
+        Course c = new Course("CS", "150", offerings);
+
+        assertThat(c.getCRSE(), is("150"));
+        assertThat(c.getSubject(), is("CS"));
+        assertTrue(c.getOfferingList().isEmpty());
+    }
+
+    @Test
+    public void testsetCRSE() {
+        Offering o = new Offering();
+        ArrayList<Offering> offerings = new ArrayList<Offering>();
+
+        Course c = new Course("CS", "150", offerings);
+        c.setCRSE("350");
+
+        assertThat(c.getCRSE(), is("350"));
+        assertThat(c.getSubject(), is("CS"));
+        assertTrue(c.getOfferingList().isEmpty());
+
+    }
+
+    @Test
+    public void testSetSub() {
+        Offering o = new Offering();
+        ArrayList<Offering> offerings = new ArrayList<Offering>();
+
+        Course c = new Course("CS", "150", offerings);
+        c.setSubject("CYSE");
+
+        assertThat(c.getCRSE(), is("150"));
+        assertThat(c.getSubject(), is("CYSE"));
+        assertTrue(c.getOfferingList().isEmpty());
+    }
+
+    @Test
+    public void testAddOfferings() {
+        Enrollment e = new Enrollment();
+        Offering o = new Offering(10, "Zeil", 10, e);
+        Offering o1 = new Offering(20, "Kennedy", 20, e);
+        ArrayList<Offering> offerings = new ArrayList<Offering>();
+
+        Course c = new Course("CS", "150", offerings);
+        c.addOffering(o);
+        c.addOffering(o1);
+
+        assertThat(c.getCRSE(), is("150"));
+        assertThat(c.getSubject(), is("CS"));
+        assertThat(c.getOfferingList(), contains(o, o1));
+    }
+
 }
