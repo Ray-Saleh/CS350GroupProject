c12b6bcfe87ae5dc674dfb7ff3ee288e55e241ba
Scott Atkins <satki002@odu.edu>
2023-04-07 23:04:30 -0400

    Removed Unsured Imports

--- project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
@@ -1,6 +1,6 @@
 package edu.odu.cs.cs350.pne;
 
-import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
