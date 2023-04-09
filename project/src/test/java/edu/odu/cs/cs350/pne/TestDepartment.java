bf62a538e9db6ffb88bab78e98bb129dcf8f5881
Prestin Bell <prestin97@gmail.com>
2023-04-08 23:27:33 -0400

    testDepartment

--- project/src/test/java/edu/odu/cs/cs350/pne/TestDepartment.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestDepartment.java
@@ -1,4 +1,5 @@
-/* package edu.odu.cs.cs350.pne;
+package edu.odu.cs.cs350.pne;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,47 +9,40 @@ import static org.hamcrest.Matchers.*;
 import edu.odu.cs.cs350.pne.*;
 
 public class TestDepartment {
-    
- 
-    @Test
-    public void testConstructor()
-    {
-       // Department depart = new Department();
-        //depart.readCSV();
-        //depart.getEnrollment();
 
-    }
-
-    @Test
-    public void testSummaryReport(){
-        
-    }
-
-    @Test
-    public void testSmoothCurving(){
+    @Test // needs to be changed
+    public void testSmoothCurving() {
         Department d = new Department();
-        int[] input1 = {1, 2, 3, 4, 5};
-        int[] expectedOutput1 = {1, 2, 3, 4, 4};
+        int[] input1 = { 1, 2, 3, 4, 5 };
+        int[] expectedOutput1 = { 1, 2, 3, 4, 4 };
         assertArrayEquals(expectedOutput1, d.smoothCurve(input1, 1));
 
-        int[] input2 = {1, 2, 3, 4};
-        int[] expectedOutput2 = {1, 2, 3, 3};
+        int[] input2 = { 1, 2, 3, 4 };
+        int[] expectedOutput2 = { 1, 2, 3, 3 };
         assertArrayEquals(expectedOutput2, d.smoothCurve(input2, 1));
 
-        int[] input3 = {1, 2, 3, 4, 5, 6};
-        int[] expectedOutput3 = {1, 2, 3, 4, 5, 5};
+        int[] input3 = { 1, 2, 3, 4, 5, 6 };
+        int[] expectedOutput3 = { 1, 2, 3, 4, 5, 5 };
         assertArrayEquals(expectedOutput3, d.smoothCurve(input3, 1));
 
-        int[] input4 = {1, 2, 3, 4, 5};
-        int[] expectedOutput4 = {1, 2, 3, 4, 4};
+        int[] input4 = { 1, 2, 3, 4, 5 };
+        int[] expectedOutput4 = { 1, 2, 3, 4, 4 };
         assertArrayEquals(expectedOutput4, d.smoothCurve(input4, 1));
 
-        int[] input5 = {1, 2, 3, 4, 5, 6};
-        int[] expectedOutput5 = {1, 2, 3, 4, 5, 5};
+        int[] input5 = { 1, 2, 3, 4, 5, 6 };
+        int[] expectedOutput5 = { 1, 2, 3, 4, 5, 5 };
         assertArrayEquals(expectedOutput5, d.smoothCurve(input5, 1));
 
-        
+    }
+
+    // @TODO
+    @Test
+    public void testMergeSections() {
+        int i = 1;
+        int i1 = 1;
+        assertThat(i + i1, is(2));
+
     }
 
 }
-*/
+
