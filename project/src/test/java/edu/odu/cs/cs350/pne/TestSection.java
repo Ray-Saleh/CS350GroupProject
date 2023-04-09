ba8d514063008dc379ffd4ff40e71f309e67ac45
Prestin Bell <prestin97@gmail.com>
2023-04-08 23:18:14 -0400

    TestSection

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
@@ -1,37 +1,55 @@
-/*package edu.odu.cs.cs350.pne;
+
+package edu.odu.cs.cs350.pne;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import java.util.ArrayList;
 
 public class TestSection {
+
     @Test
-    public void TestSection() {
+    public void testConstructor() {
         Section s = new Section();
-        assertThat(s.getcrn(), is(0));
-        assertThat(s.getSub(), is(""));
-        assertThat(s.getCRSE(), is(000));
-        // assertThat(s.getLINK(), is(0));
-        // assertThat(s.getXLSTCap(), is(0));
+
+        assertThat(s.getLink(), is(""));
+        assertTrue(s.getOfferingList().isEmpty());
     }
 
     @Test
-    public void testCopyConstructor() {
-        Course c = new Course("cs", 150);
-        Enrollment e = new Enrollment(10, 10, 10, 10, 10, 10, c);
-        Section s = new Section(1010, e, c);
+    public void testCConstructor() {
+        ArrayList<Offering> o = new ArrayList<Offering>();
+        Section s = new Section(o, "Apples");
 
-        assertThat(s.getcrn(), is(1010));
-        assertThat(s.getSub(), is("cs"));
-        assertThat(s.getCRSE(), is(150));
-        assertThat(s.getLINK(), is(10));
-        assertThat(s.getXLSTCap(), is(10));
+        assertThat(s.getLink(), is("Apples"));
+        assertTrue(s.getOfferingList().isEmpty());
     }
 
+    @Test
     public void testSetLinked() {
+        ArrayList<Offering> o = new ArrayList<Offering>();
+        Section s = new Section(o, "Apples");
+        s.setLINK("Boatz n Hoez");
+        assertThat(s.getLink(), is("Boatz n Hoez"));
+        assertTrue(s.getOfferingList().isEmpty());
 
+    }
+
+    @Test
+    public void testAddOffering() {
+        Enrollment e = new Enrollment();
+        Offering f = new Offering(69, "Zeil", 420, e);
+        Offering f1 = new Offering(420, "Kennedy", 69, e);
+
+        ArrayList<Offering> o = new ArrayList<Offering>();
+        Section s = new Section(o, "Apples");
+        s.addOffering(f);
+        s.addOffering(f1);
+        assertThat(s.getLink(), is("Apples"));
+
+        // check that s.getOfferingList() contains(f,f1);
+        assertThat(s.getOfferingList(), contains(f, f1));
     }
 }
-*/
