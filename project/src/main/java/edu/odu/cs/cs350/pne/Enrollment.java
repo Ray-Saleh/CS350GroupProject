7755dcc7277d9c304166223bb0e4a61f27c950b2
Scott Atkins <satki002@odu.edu>
2023-04-24 06:08:29 -0400

    Enrollment:Removed old code &Formatted linespacing

--- project/src/main/java/edu/odu/cs/cs350/pne/Enrollment.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Enrollment.java
@@ -4,14 +4,16 @@ public class Enrollment {
 
     //Cross list cap: Maximum number of students that can enroll in this section
     int XLSTCap;
+
     //Number of students enrolled in this section
     int ENR;
-    //Used to associate labs with the lecture. All link codes consist of an upper-case char and a single digit int
-    //String LINK;
+
     //Cross list cap: IDs which sections of a course belong to a single offering. (Array of Courses that match)
     String XLSTGroup;
+
     //The maximum number of students that can enroll in the offering to which this section belongs.
     int OVERALLCAP;
+    
     //Cross listed enrollment
     int XLSTENR;
 
@@ -34,35 +36,32 @@ public class Enrollment {
         
     }
     ///Accesors 
-    // public int getXLSTCap() {
-    //     return XLSTCap;
-    // }
+
     public int getENR() {
         return ENR;
     }
-    // public String getXLSTGroup(){
-    //     return XLSTGroup;
-    // }
+ 
     public int getOVERALLCAP(){
         return OVERALLCAP;
     }
-    // public int getXLSTENR() {
-    //     return XLSTENR;
-    // }
 
     ///Mutators
     public void setXLSTCap(int inXLSTCap) {
         XLSTCap = inXLSTCap;
     }
+
     public void setENR(int inENR) {
         ENR = inENR;
     }
+
     public void setXLSTGroup(String inXLSTGroup){
        XLSTGroup = inXLSTGroup;
     }
+
     public void setOVERALLCAP(int inOVERALLCAP){
         OVERALLCAP = inOVERALLCAP;
     }
+
     public void setXLSTENR(int inXLSTENR) {
        XLSTENR = inXLSTENR;
     }
