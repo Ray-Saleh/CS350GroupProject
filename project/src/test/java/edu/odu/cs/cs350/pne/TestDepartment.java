cf4402da0af0833973f5c55cdaeedd97246e99b9
Scott Atkins <satki002@odu.edu>
2023-04-24 05:44:56 -0400

    TestDepartment: Removed Unused Imports

--- project/src/test/java/edu/odu/cs/cs350/pne/TestDepartment.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestDepartment.java
@@ -1,16 +1,13 @@
 package edu.odu.cs.cs350.pne;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
+import java.util.ArrayList
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-    import edu.odu.cs.cs350.pne.*;
 
 public class TestDepartment {
 
