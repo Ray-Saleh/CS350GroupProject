f0cb338a32c572e4b7c754f74ee5c1b8e13a4d64
Scott Atkins <satki002@odu.edu>
2023-04-24 06:03:25 -0400

    Snapshot: Removed Old Code

--- project/src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
@@ -45,24 +45,4 @@ public ArrayList<Course> getCourseList(){
   return courseList;
 }
 
-//setters
-
-    
 }
-/*
-                            //idea of how to write in data from files found from   https://www.w3schools.com/java/java_files_create.asp
-public class WriteToFile {
-  public static void main(String[] args) {
-    try {
-      FileWriter myWriter = new FileWriter("filename.txt");
-      myWriter.write("Files in Java might be tricky, but it is fun enough!");
-      myWriter.close();
-      System.out.println("Successfully wrote to the file.");
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-  }
-}
-
-*/
