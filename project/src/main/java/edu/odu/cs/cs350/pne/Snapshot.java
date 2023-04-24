8ac9d3030c59676ab38deed4bbb4fd8c644353ee
Scott Atkins <satki002@odu.edu>
2023-04-24 06:04:42 -0400

    SnapShot: Formatted LineSpace and Indentation

--- project/src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
@@ -2,9 +2,9 @@ package edu.odu.cs.cs350.pne;
 import java.util.ArrayList;
 public class Snapshot {
 
-
   ArrayList<Course> courseList;
   String fileName;
+
   public Snapshot(){
   
     fileName = "emptySnapshot";
@@ -17,32 +17,33 @@ public class Snapshot {
   }
 
 
-public void setFileName(String inName)
-{
-  fileName = inName;
-}
-
-public String getFileName(){
-  return fileName;
-}
-
-public void addCourse(Course addCourse){
-  courseList.add(addCourse);
-}
-public Course getCourse(String inCRSE){
-  for(int i= 0; i<courseList.size(); i ++)
+  public void setFileName(String inName)
   {
+    fileName = inName;
+  }
+
+  public String getFileName(){
+    return fileName;
+  }
+
+  public void addCourse(Course addCourse){
+    courseList.add(addCourse);
+  }
+
+  public Course getCourse(String inCRSE){
     
-     if( courseList.get(i).getCRSE().equals(inCRSE))
-     {
-      return courseList.get(i);
-     }
-  }   
-  return null;
-}
+    for(int i= 0; i<courseList.size(); i ++)
+    {
+      
+      if( courseList.get(i).getCRSE().equals(inCRSE))
+      {
+        return courseList.get(i);
+      }
+    }   
+    return null;
+  }
 
-public ArrayList<Course> getCourseList(){
-  return courseList;
-}
-
+  public ArrayList<Course> getCourseList(){
+    return courseList;
+  }
 }
