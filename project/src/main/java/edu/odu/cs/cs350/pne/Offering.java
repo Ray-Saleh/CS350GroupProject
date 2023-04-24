809097df61ec191bb99a0190a1a583a783c97d62
Scott Atkins <satki002@odu.edu>
2023-04-24 06:06:59 -0400

    Offering: Removed Old Code

--- project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
@@ -31,17 +31,9 @@ public class Offering {
         return this.professor;
     }
 
-    // public int getNumSections() {
-    //     return this.numSections;
-    // }
-
     public void setProfessor(String string) {
         this.professor = string;
     }
-
-    // public void setNumSections(int i) {
-    //     this.numSections = i;
-    // }
 
     public void setCRN(int inCRN) {
         this.crn = inCRN;
